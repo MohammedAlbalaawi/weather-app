2fcd3b650ca063f2eb7a8e144449007d0cc0056c
Mohammed Albalaawi <m.cip@live.com>
2021-12-04 20:54:26 +0200

    'change'

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import './Weather.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Weather() {
 
@@ -14,34 +15,54 @@ function Weather() {
             }).catch((err) => {
                 console.log(err)
             });
+
     }
 
 
+    // function wData() {
+    //     if(weather !== "" ){
+    //     return (
+    //         <div className="cdata">
+    //             <p>{weather.name}</p>
+    //             <p>{weather.main.temp} °C</p>
+    //             <p>{weather.weather[0].main}</p>
+    //         </div>
+    //     )}else{
+    //         return ""
+    //     }
+    // }
     function wData() {
-        if(weather !== "" ){
-        return (
-            <div className="cdata">
-                <p>{weather.name}</p>
-                <p>{weather.main.temp} °C</p>
-                <p>{weather.weather[0].main}</p>
-            </div>
-        )}else{
-            return ""
+        if (weather !== "") {
+            return (
+                <div className="contanier">
+                    <div className='row bg-light'>
+                        <p className="badge  text-dark  fs-1">{weather.name}</p>
+                        <p className="badge text-dark  fs-1">{weather.main.temp} °C</p>
+                        <p className="badge text-dark  fs-1">{weather.weather[0].main}</p>
+                        <div>
+                            <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                                alt='icon' width="100" height="100" />
+                        </div>
+                    </div>
+                </div>
+            )
+        } else {
+            return <div className="text-uppercase"><br />"Please Choose a Country ..,"</div>
         }
     }
 
     return (
         <div className="container">
-            <div className="cities">
-                <p onClick={() => getWeather('Jeruzalem')}>Jeruzalem</p>
-                <p onClick={() => getWeather('Gaza')}>Gaza</p>
-                <p onClick={() => getWeather('Cairo')}>Cairo</p>
-                <p onClick={() => getWeather('Tunis')}>Tunis</p>
-                <p onClick={() => getWeather('Baghdad')}>Baghdad</p>
-            </div>
-            <div >
+
+            <p className='btn btn-outline-dark' onClick={() => getWeather('Jeruzalem')}>Jeruzalem</p>
+            <p className='btn btn-outline-dark' onClick={() => getWeather('Gaza')}>Gaza</p>
+            <p className='btn btn-outline-dark' onClick={() => getWeather('Cairo')}>Cairo</p>
+            <p className='btn btn-outline-dark' onClick={() => getWeather('Tunis')}>Tunis</p>
+            <p className='btn btn-outline-dark' onClick={() => getWeather('Baghdad')}>Baghdad</p>
+
+
             {wData()}
-            </div>
+
         </div>
     )
 
